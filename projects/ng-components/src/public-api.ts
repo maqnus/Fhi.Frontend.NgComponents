87c27385cc92f918ca933b348813b98164183791
Magnus Kjelland <magnus@kjelland.com>
2021-03-16 11:20:09 +0100

    Expose component in public api

--- projects/ng-components/src/public-api.ts
+++ projects/ng-components/src/public-api.ts
@@ -10,3 +10,6 @@ export * from './lib/fhi-tab-bar/fhi-tab-bar.module';
 
 export * from './lib/fhi-accordion/fhi-accordion.component';
 export * from './lib/fhi-accordion/fhi-accordion.module';
+
+export * from './lib/fhi-datepicker/fhi-datepicker.component';
+export * from './lib/fhi-datepicker/fhi-datepicker.module';
